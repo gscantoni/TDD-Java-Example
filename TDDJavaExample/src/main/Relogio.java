30f5ac0adce1fc5ff4276f6abf365d7cdda80134
Guilherme Cantoni <104282001+gscantoni@users.noreply.github.com>
2023-08-30 22:59:32 -0300

    Update Relogio.java
    Pode ser programado com horas, minutos e segundos.

--- TDDJavaExample/src/main/Relogio.java
+++ TDDJavaExample/src/main/Relogio.java
@@ -1,1 +1,34 @@
+package main;
+
+public class Relogio {
+	
+	private int segundo;
+    private int minuto;
+    private int hora;
+    private String intervaloInicio;
+    private String intervaloFim;
+
+    public void setHora(int hora) {
+        this.hora = hora;
+    }
+
+    public int getHora() {
+        return hora;
+    }
+
+    public void setMinuto(int minuto) {
+        this.minuto = minuto;
+    }
+
+    public int getMinuto() {
+        return minuto;
+    }
+
+    public void setSegundo(int segundo) {
+        this.segundo = segundo;
+    }
+
+    public int getSegundo() {
+        return segundo;
+    }
 

01ea3a119c8d7e87e061bcd5ca2c93afe95edc22
Guilherme Cantoni <104282001+gscantoni@users.noreply.github.com>
2023-08-30 22:58:32 -0300

    Update RelogioTest.java
    Pode ser programado com horas, minutos e segundos

--- TDDJavaExample/src/test/RelogioTest.java
+++ TDDJavaExample/src/test/RelogioTest.java
@@ -1,1 +1,28 @@
+package test;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import main.Relogio;
+
+class RelogioTest {
+
+    @Test
+    void testSetHora() {
+        Relogio relogio = new Relogio();
+        relogio.setHora(2);
+        assertEquals(2, relogio.getHora());
+    }
+    @Test
+    void testSetMinuto() {
+        Relogio relogio = new Relogio();
+        relogio.setMinuto(25);
+        assertEquals(25, relogio.getMinuto());
+    }
+
+    @Test
+    void testSetSegundo() {
+        Relogio relogio = new Relogio();
+        relogio.setSegundo(50);
+        assertEquals(50, relogio.getSegundo());
+    }

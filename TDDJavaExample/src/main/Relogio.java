6f94cc5beaa3d09719f528d52780595e5fa9bcce
Guilherme Cantoni <104282001+gscantoni@users.noreply.github.com>
2023-08-30 23:27:37 -0300

    Update Relogio.java
    Não passa o tempo sozinho, mas atualiza corretamente o horário quando recebe
    indicação de passagem do tempo do Sistema Operacional

--- TDDJavaExample/src/main/Relogio.java
+++ TDDJavaExample/src/main/Relogio.java
@@ -74,3 +74,10 @@ public class Relogio {
     public String getHoraFormato24h() {
         return String.format("%02d:%02d:%02d", hora, minuto, segundo);
     }
+	
+    public String getHoraFormatoAMPM() {
+        String ampm = hora < 12 ? "AM" : "PM";
+        int horaFormato12h = hora > 12 ? hora - 12 : hora;
+        return String.format("%d:%02d:%02d %s", horaFormato12h, minuto, segundo, ampm);
+    }
+}
